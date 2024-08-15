b2e4302345faf0483adbc019dadb3fe1c0c9072d
Rolislaff <nikita.rolis1@gmail.com>
2024-08-15 20:15:48 +0300

    Fixed timeout script

--- con_host.sh
+++ con_host.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
 ip=(ipv4)
-action=$(nmap -Pn -p 22 $ip | awk '/tcp/{ print $2 }' > output3.txt)
 val_negative=$(cat output3.txt | grep filtered | wc -l)
 val_agree=$(cat output3.txt | grep open | wc -l)
 start=`date +%s`
 
 while true; do
-    $action
+    nmap -Pn -p 22 $ip | awk '/tcp/{ print $2 }' > output3.txt
     if [ $val_agree -ge 1 ]; then
         echo "Connection establish!"
         end=`date +%s`
         echo "Connection was established after: $(($end-$start)) seconds."
         exit
-    elif
+    else
         echo "Not connected"
         cat output3.txt
         sleep 1
-        if [ $(($(date +%s)-$start)) -ge 30 ]; then
+        if [ $(($(date +%s)-$start)) -ge 35 ]; then
             echo "Error Timeout"
             exit
         fi

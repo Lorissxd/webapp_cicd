e832e0c39a3d747d266a73a302498b830313a139
Rolislaff <nikita.rolis1@gmail.com>
2024-08-16 15:55:53 +0300

    Fixing timeout problem#2

--- con_host.sh
+++ con_host.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
 ip=(ipv4)
-val_negative=$(cat output3.txt | grep filtered | wc -l)
-val_agree=$(cat output3.txt | grep open | wc -l)
 start=`date +%s`
 
 while true; do
-    nmap -p 22 $ip | awk '/tcp/{ print $2 }' > output3.txt
-    if [ $val_agree -ge 1 ]; then
-        echo "Connection establish!"
-        end=`date +%s`
-        echo "Connection was established after: $(($end-$start)) seconds."
-        exit
-    else
-        echo "Not connected"
-        cat output3.txt
-        sleep 1
-        if [ $(($(date +%s)-$start)) -ge 35 ]; then
+    nmap -Pn -p 22 $ip | awk '/tcp/{ print $2 }' > output3.txt
+    if [ $? -eq 0 ]; then
+        val_agree=$(cat output3.txt | grep -i open | wc -l)
+        if [ $val_agree -eq 1 ]; then
+            end=`date +%s`
+            echo "Connection establish!"
+            echo "Connection was established after: $(($end-$start)) seconds."
+            exit
+        else
+            echo "Not connected"
+            cat output3.txt
+            sleep 1
+        fi
+
+
+    elif [ $(($(date +%s)-$start)) -ge 35 ]; then
             echo "Error Timeout"
             exit
-        fi
     fi
 done

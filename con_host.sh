28b3eef26e07af88c82c1280a160ab204aff6d74
Rolislaff <nikita.rolis1@gmail.com>
2024-08-21 19:23:52 +0300

    Troubleshooting ssh(from nmap switched to netstat)#16

--- con_host.sh
+++ con_host.sh
@@ -4,9 +4,10 @@ ip=(ipv4)
 start=`date +%s`
 
 while true; do
-    nmap -Pn -p 22 $ip | awk '/tcp/{ print $2 }' > output3.txt
+    nmap -Pn -p 443 $ip | awk '/tcp/{ print $2 }' > output3.txt
     if [ $? -eq 0 ]; then
         val_agree=$(cat output3.txt | grep -i open | wc -l)
+        val_error=$(cat output3.txt | grep -E 'filtered|closed')
         if [ $val_agree -eq 1 ]; then
             end=`date +%s`
             cat output3.txt
@@ -15,9 +16,8 @@ while true; do
             sleep 5
             exit
         else
-            echo "Not connected!"
-            cat output3.txt
-            sleep 2
+            echo "Waiting for connection status now is: $val_error"
+            sleep 1
         fi
 
 

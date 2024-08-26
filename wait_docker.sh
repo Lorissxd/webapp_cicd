e70405ed7ed388e4bab50ecf29a7330992644947
Rolislaff <nikita.rolis1@gmail.com>
2024-08-26 18:08:33 +0300

    Waiting in server for docker start#11

--- wait_docker.sh
+++ wait_docker.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
 while true; do
-    docker ps 2>&1 >/dev/null
-    exit_code=$?
-    docker ps 2>&1 | grep -i "CONTAINER ID" >/dev/null 2>/dev/null
-    exit_code1=$?
-    docker ps 2>&1 | grep -i "permission denied" >/dev/null 2>/dev/null
-    exit_code2=$?
+
     if [ $exit_code -eq 0 ]; then
         if [ $exit_code1 -eq 0 ]; then
             echo "Docker sucessfully found on the server"
@@ -18,7 +13,6 @@ while true; do
 
     elif [ $exit_code2 -eq 0 ]; then
         echo "Done, docker have been installed"
-        sudo reboot
         exit
     fi
 done
